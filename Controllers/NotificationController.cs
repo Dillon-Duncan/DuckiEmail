8a41485cdedb18fa92fecf7241dc0dd7fd2ffbe7
Dillon-Duncan <145899893+Dillon-Duncan@users.noreply.github.com>
2024-05-22 10:45:27 +0200

    Add files via upload

---
+++ Controllers/NotificationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DuckiEmail.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private readonly ISubject eventNotifier;
+
+        public NotificationController(ISubject eventNotifier)
+        {
+            eventNotifier = eventNotifier;
+        }
+
+        public IActionResult Subscribe()
+        {
+            var observer = new EmailObserver();
+            eventNotifier.Attach(observer);
+            return Ok("Subscribed");
+        }
+
+        public IActionResult Unsubscribe()
+        {
+            var observer = new EmailObserver();
+            eventNotifier.Detach(observer);
+            return Ok("Unsubscribed");
+        }
+
+        public IActionResult Notify(string message)
+        {
+            eventNotifier.Notify(message);
+            return Ok("Notified");
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DuckiEmail.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
